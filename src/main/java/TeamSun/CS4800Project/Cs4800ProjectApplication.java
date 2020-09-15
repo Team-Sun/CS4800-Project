aec794fae1da1049aa99225c8567c8453def5d4e
Andr3wD <37052064+Andr3wD@users.noreply.github.com>
2020-09-15 11:57:11 -0700

    Initial commit with spring boot starter.

---
+++ src/main/java/TeamSun/CS4800Project/Cs4800ProjectApplication.java
@@ -0,0 +1,13 @@
+package TeamSun.CS4800Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs4800ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs4800ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TeamSun/CS4800Project/Cs4800ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package TeamSun.CS4800Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs4800ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

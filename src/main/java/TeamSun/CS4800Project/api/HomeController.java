ef1b3fdaffbb844b91bdce9770cb5ce475be4749
Andr3wD <37052064+Andr3wD@users.noreply.github.com>
2020-09-15 22:41:51 -0700

    Added basic get HTML API for part 3 of assignment.

--- src/main/java/TeamSun/CS4800Project/api/HomeController.java
+++ src/main/java/TeamSun/CS4800Project/api/HomeController.java
@@ -1,5 +1,17 @@
 package TeamSun.CS4800Project.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("")
+@RestController
 public class HomeController {
+	
+	@GetMapping
+	public String getPage() {
+		return "something simple";
+	}
+	
 
 }

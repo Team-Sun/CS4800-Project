c6c7118990cbd8953d120142986536007615d487
Jonathan Chang <jonathanjc1118@gmail.com>
2020-09-20 13:16:41 -0700

    Changed

--- src/main/java/TeamSun/CS4800Project/api/HomeController.java
+++ src/main/java/TeamSun/CS4800Project/api/HomeController.java
@@ -1,7 +1,9 @@
 package TeamSun.CS4800Project.api;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping

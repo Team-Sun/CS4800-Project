c6da63e925f0f900d7f9e07abb81479fac17e604
Andr3wD <37052064+Andr3wD@users.noreply.github.com>
2020-09-24 12:07:41 -0700

    Revert "adding A4"
    This reverts commit f96f037a06cd58f776f405677ee2bb2337048963.
    
    Revert accidental repo duplication.

--- src/main/java/TeamSun/CS4800Project/api/WebController.java
+++ src/main/java/TeamSun/CS4800Project/api/WebController.java
@@ -1,14 +1,8 @@
 package TeamSun.CS4800Project.api;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Date;
 
 import com.google.common.base.Joiner;
-
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.math3.fraction.Fraction;
 import org.joda.time.DateTime;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,16 +31,5 @@ public class WebController {
 	{
 		Fraction f = new Fraction(1,2);
 		return f.doubleValue();
-	}
-
-	@GetMapping("/IOExample")
-	public String IOExample() throws MalformedURLException, IOException
-	{
-		InputStream in = new URL( "https://commons.apache.org" ).openStream();
-	 	try {
-	   		return( IOUtils.toString( in, "UTF-8" ) );
-	 	} finally {
-	   		IOUtils.close(in);
-	 	}
 	}
 }

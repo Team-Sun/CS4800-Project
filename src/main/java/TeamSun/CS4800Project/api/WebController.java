7816322594de10ce409d7e12cafa964028e1a454
Nicky Hysmith <nickyhysmith@gmail.com>
2020-09-24 13:05:33 -0700

    re-adding A4

--- src/main/java/TeamSun/CS4800Project/api/WebController.java
+++ src/main/java/TeamSun/CS4800Project/api/WebController.java
@@ -1,8 +1,14 @@
 package TeamSun.CS4800Project.api;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Date;
 
 import com.google.common.base.Joiner;
+
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.math3.fraction.Fraction;
 import org.joda.time.DateTime;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,5 +37,16 @@ public class WebController {
 	{
 		Fraction f = new Fraction(1,2);
 		return f.doubleValue();
+	}
+	
+	@GetMapping("/IOExample")
+	public String IOExample() throws MalformedURLException, IOException
+	{
+		InputStream in = new URL( "https://commons.apache.org" ).openStream();
+	 	try {
+	   		return( IOUtils.toString( in, "UTF-8" ) );
+	 	} finally {
+	   		IOUtils.close(in);
+	 	}
 	}
 }

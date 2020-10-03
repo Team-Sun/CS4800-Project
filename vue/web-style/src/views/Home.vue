e19ba48708e82a27769f276fd41e12ee8c78da86
Jonathan Chang <jonathanjc1118@gmail.com>
2020-10-02 20:40:58 -0700

    Updated Homepage

--- vue/web-style/src/views/Home.vue
+++ vue/web-style/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <Homepage msg="This is a Homepage"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Homepage from '@/components/Homepage.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Homepage
   }
 }
 </script>

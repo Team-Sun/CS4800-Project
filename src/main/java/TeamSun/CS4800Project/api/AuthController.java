28ca722b1549db8b9665b0ce04800d571b799c19
Andr3wD <37052064+Andr3wD@users.noreply.github.com>
2020-10-07 10:31:39 -0700

    Small changes. Added AuthController

---
+++ src/main/java/TeamSun/CS4800Project/api/AuthController.java
@@ -0,0 +1,10 @@
+package TeamSun.CS4800Project.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthController {
+
+}

40862f4d5bc9677b13abd54255496d23782475d9
Nicky Hysmith <nickyhysmith@gmail.com>
2020-10-08 11:14:59 -0700

    Homepage

--- src/vue/src/App.vue
+++ src/vue/src/App.vue
@@ -1,80 +1,45 @@
 <template>
-  <div id="app">
-    <div class="flex-header-container">
-      <div id="logo">
-        <router-link to="/"><img src="./assets/note-logo2.png" alt="Bronco Notes"></router-link>
+    <div id ="app">
+      <div id ="navBar">
+        <Header/>
       </div>
-      <div id="nav">
-        <router-link to="/">Home</router-link>
-        <router-link to="/search">Search</router-link>
-        <router-link to="/about">About</router-link>
-        <!-- <router-link to="/login">Sign in</router-link> -->
-      </div>
-      <!-- Log in button -->
-      <div>
-        <b-dropdown id="login-form" right text="Log in" ref="dropdown" class="m-2">
-          <b-dropdown-form>
-            <b-form-group label="Email" label-for="dropdown-form-email" @submit.stop.prevent>
-              <b-form-input
-                id="email"
-                size="sm"
-                placeholder="email@example.com"
-              ></b-form-input>
-            </b-form-group>
-
-            <b-form-group label="Password" label-for="dropdown-form-password">
-              <b-form-input
-                id="password"
-                type="password"
-                size="sm"
-                placeholder="Password"
-              ></b-form-input>
-            </b-form-group>
-
-            <b-form-checkbox class="mb-3">Remember me</b-form-checkbox>
-            <b-button variant="primary" size="sm" @click="onClick">Sign In</b-button>
-          </b-dropdown-form>
-          <b-dropdown-divider></b-dropdown-divider>
-          <b-dropdown-item-button>New around here? Sign up</b-dropdown-item-button>
-          <b-dropdown-item-button>Forgot Password?</b-dropdown-item-button>
-        </b-dropdown>
+      <div id="wrapper">
+        <ClassNotePanel/>
+        <Upload/>
       </div>
     </div>
-    <router-view/>
-  </div>
-</template>
+  </template>
 
-<style lang="scss">
-#app {
-  font-family: "Comic Sans MS", cursive, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-.flex-header-container {
-  display: flex;
-  background-color: rgba(0, 0, 0, 0.8);
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
+<script>
+import Header from './components/Header.vue'
+import ClassNotePanel from './components/ClassAndNote.vue'
+import Upload from './components/Upload.vue'
+export default {
+  name: 'App',
+  data()
+  {
 
-}
-#logo img {
-  display: block;
-  height: auto;
-  width: auto;
-  max-height: 60px;
-}
-#nav {
-  padding: 25px;
-  a {
-    margin-left: 80px;
-    font-weight: 800;
-    color: white;
-    &.router-link-exact-active {
-      color: #6495ED;
-    }
+  },
+  components: {
+  Header,
+  ClassNotePanel,
+  Upload
+
+
   }
+}
+</script>
+
+<style>
+#app {
+  background:#1111;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  
+}
+
+#wrapper{
+  display:grid;
+  grid-template-columns: 30% 70%;
+  grid-auto-rows: minmax(500px, auto);
 }
 </style>
---
+++ src/vue/src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+      <div id ="header">
+        <h1> Welcome to Bronco Notes</h1>
+        <form>
+          <input type="search">
+          <button v-on:click="displayQuery">Search</button>
+        </form>
+
+        <nav>
+          <div class="menu-items"><a href="#">Home</a></div>
+          <div class="menu-items"><a href="#">Upload</a></div>
+          <div class="menu-items"><a href="#">About</a></div>
+          <div class="menu-items"><a href="#">Login</a></div>
+          <div class="menu-items"><a href="#">Sign Up</a></div>
+         
+        </nav>
+
+      </div>
+  </template>
+
+<script>
+
+export default {
+  name: 'Header',
+  setup()
+  {
+    function displayQuery()
+    {
+
+    }
+
+    return{
+    displayQuery,
+    }
+  }
+}
+</script>
+
+<style>
+#header {
+  background:#006400;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  margin-top: 60px;
+}
+nav{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+}
+nav .menu-items{
+  color: white;
+  padding: 10px 20px;
+  text-align: center;
+}
+
+</style>

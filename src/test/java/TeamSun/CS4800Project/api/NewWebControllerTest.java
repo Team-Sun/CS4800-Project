12672dc47cbefb309e6f588bd241a5d7c06cdcc3
Jonathan Chang <jonathanjc1118@gmail.com>
2020-10-07 23:48:27 -0700

    Added A6 JUnit Test - Jonathan

--- src/test/java/TeamSun/CS4800Project/api/NewWebControllerTest.java
+++ src/test/java/TeamSun/CS4800Project/api/NewWebControllerTest.java
@@ -8,6 +8,7 @@ public class NewWebControllerTest {
 	private NewWebController nwc = new NewWebController();
 	private DateTime dt = new DateTime();
 	
+	// This is a test 
 	@Test
 	public void testResult() {
 		boolean res = nwc.isAfterPayDay(dt);

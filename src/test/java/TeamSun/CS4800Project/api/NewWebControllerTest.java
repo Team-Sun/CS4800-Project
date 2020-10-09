a628c5e9d80bb09502081304486c49bbcba19c4d
jonathan1118 <60635808+jonathan1118@users.noreply.github.com>
2020-10-09 08:56:00 -0700

    Create NewWebControllerTest.java
    A6 JUnit Test - Jonathan

---
+++ src/test/java/TeamSun/CS4800Project/api/NewWebControllerTest.java
@@ -0,0 +1,16 @@
+package TeamSun.CS4800Project.api;
+
+import org.joda.time.DateTime;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NewWebControllerTest {
+	private NewWebController nwc = new NewWebController();
+	private DateTime dt = new DateTime();
+	
+	@Test
+	public void testResult() {
+		boolean res = nwc.isAfterPayDay(dt);
+		Assert.assertEquals(dt.getDayOfMonth(), res);
+	}
+}

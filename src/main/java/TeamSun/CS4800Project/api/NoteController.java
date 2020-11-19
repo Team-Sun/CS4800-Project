147ab7bd4e7f11d9e6c0f5bf9bf063896880f418
Andr3wD <37052064+Andr3wD@users.noreply.github.com>
2020-11-19 09:59:35 -0800

    Still trying to fix embed pdf

--- src/main/java/TeamSun/CS4800Project/api/NoteController.java
+++ src/main/java/TeamSun/CS4800Project/api/NoteController.java
@@ -85,13 +85,14 @@ public class NoteController {
 
 	// Thanks to
 	// https://stackoverflow.com/questions/59686660/how-to-send-generated-pdf-document-to-frontend-in-restfull-way-in-spring-boot
-	@GetMapping("/file/{id}.pdf")
+	@GetMapping("/file/{id}")
 	// @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 	@PreAuthorize("permitAll()")
 	public void getFile(@PathVariable("id") ObjectId id, HttpServletRequest request, HttpServletResponse response) {
 		// User clientUser = userService.find(request);
 		Note note = noteService.findByID(id);
 		try {
+			response.setContentType("application/pdf");
 			response.setHeader("Content-Disposition", String.format("inline; filename=\"testnote.pdf" + "\""));
 			response.getOutputStream().write(note.getFile().getData());
 		} catch (IOException e) {
